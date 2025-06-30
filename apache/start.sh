8427eb6bd50ca82f210e5acd873a358f3aab2871
Pedro Consuegra <pedroconsuegrat@gmail.com>
2025-06-30 06:46:35 -0400

    apache cache fix

--- apache/start.sh
+++ apache/start.sh
@@ -15,7 +15,8 @@ chmod 750 /var/log/apache2
 # Ensure log directory exists
 mkdir -p /var/www/html/wwwlog/Pete
 
-exec apachectl -D FOREGROUND
-
 mkdir -p /var/cache/apache2/mod_cache_disk/tmp
 chown -R www-data:www-data /var/cache/apache2
+
+exec apachectl -D FOREGROUND
+
